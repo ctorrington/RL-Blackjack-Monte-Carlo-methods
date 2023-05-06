8ea60b96911c94b67319d4780bc0306faf7d6803
ctorrington <ctorrington1@gmail.com>
2023-05-06 13:56:51 +0100

    added MC value function estimation

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ class Blackjack:
         for player_sum in range(12, 21 + 1):
             for dealer_sum in range(1, 10 + 1):
                 for usable_ace in range(0, 1 + 1):
-                    key = (player_sum, dealer_sum, usable_ace)
+                    state = (player_sum, dealer_sum, usable_ace)
 
-                    self.state_space[key] = {
+                    self.state_space[state] = {
                         'state actions': [self.ACTIONS.HIT, self.ACTIONS.STICK],
                         'state reward': 0,
                         'state value': 0,
@@ -138,7 +138,7 @@ class Blackjack:
             # print("Agent wins.")
             episode[-1]['reward'] = 1
 
-        print(f"({player_hand_value}, {dealer_hand_value}, {player_has_ace})")
+        # print(f"({player_hand_value}, {dealer_hand_value}, {player_has_ace})")
         # print(episode)
         return episode
 
@@ -146,7 +146,7 @@ class Blackjack:
         """Estimate the value function under the policy with First-visit
         Monte Carlo Prediction."""
 
-        maximum_number_of_episodes = 1
+        maximum_number_of_episodes = 10000
         gamma = 1
 
         # Loop for every episode.
@@ -158,11 +158,35 @@ class Blackjack:
             episode = list(reversed(episode))
             # episode_list.append(episode)
             expected_return = 0
-            print(episode)
-            for step in episode:
-                print(step)
-                print(f"next reward {step['reward']}")
+            # print(episode)
+            for index, step in enumerate(episode):
+                # print(step)
+                # print(f"next reward {step['reward']}")
                 expected_return = gamma * expected_return + step['reward']
+                # Check if the current state appears in the upcoming states 
+                # (the beginning states), this list is reversed.
+                state_upcoming = False
+                # print("upcoming states:")
+                for upcoming_state in range(index + 1, len(episode)):
+                    # print(f"STAAATE: {episode[upcoming_state]}")
+                    if step['state'] == episode[upcoming_state]['state']:
+                        state_upcoming = True
+                        break
+                if not state_upcoming:
+                    # print(f"Expected return G: {expected_return}.")
+                    self.state_space[step['state']]['state returns'].append(expected_return)
+                    # print(f"State returns: {self.state_space[step['state']]['state returns']}.")
+                    self.state_space[step['state']]['state value'] = sum(self.state_space[step['state']]['state returns'])
+                    # print(f"State value: {self.state_space[step['state']]['state value']}.")
+
+        # print(f"State space after {maximum_number_of_episodes} episodes:\n{self.state_space}")
+        highest_state_value = 0
+        for state in self.state_space:
+            print(f"{state}:\n{self.state_space[state]}")
+            if self.state_space[state]['state value'] > highest_state_value:
+                highest_state_value = self.state_space[state]['state value']
+
+        print(highest_state_value)
 
 
 
